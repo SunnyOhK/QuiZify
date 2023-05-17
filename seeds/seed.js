ca5c9ab4f2c4c646f6a8aa1ea463c601c187bbbd
garfias06 <garfias06_23@outlook.com>
2023-05-16 22:49:33 -0500

    user table working

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,40 +1,17 @@
-// const sequelize = require('../config/connection');
-// const Artist = require('../models/artist.js');
-// const fetch = require('isomorphic-fetch');
-
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
-
-//   const artistData = await fetchArtistsFromAPI();
-//   await Artist.bulkCreate(artistData);
-// };
-
-// async function fetchArtistsFromAPI() {
-//   const response = await fetch('https://api.deezer.com/chart/0/artists?limit=50');
-//   const data = await response.json();
-
-//   const artists = data.data.map((artist) => ({
-//     name: artist.name,
-//     picture: artist.picture,
-//     link: artist.link,
-//   }));
-//   return artists;
-// }
-
-// seedDatabase();
-
-
 const sequelize = require('../config/connection');
-const {User, Artist, Song}=require('../models/index')
-// const Artist = require('../models/artist.js');
-// const Song = require('../models/song');
-const userData=require('./userData.json');
+// const { User, Artist, Song } = require('../models/index');
+const Artist = require('../models/artist.js');
+const Song = require('../models/song');
+const User = require('../models/user');
+const userData = require('./userData.json');
 
 const fetch = require('isomorphic-fetch');
 
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData);
 
   const artistData = await fetchArtistsFromAPI();
   await Artist.bulkCreate(artistData);
@@ -43,7 +20,7 @@ const seedDatabase = async () => {
   await Song.bulkCreate(songData);
 
   // const userData = await fetchSongsFromAPI();
-  await User.bulkCreate(userData);
+
 };
 
 async function fetchArtistsFromAPI() {
@@ -56,32 +33,35 @@ async function fetchArtistsFromAPI() {
     picture: artist.picture,
     link: artist.link,
   }));
+  // console.log(artists)
   return artists;
 }
 
 async function fetchSongsFromAPI() {
   const response = await fetch('https://api.deezer.com/chart/0/artists?limit=50');
   const data = await response.json();
+  // console.log(data)
 
   const artist = data.data.map((artist) => ({
-    artist_id: artist.id
+    id: artist.id
   }));
- const randomSongs=`https://api.deezer.com/artist/${artist}/top?limit=4`;
+  console.log(artist)
+  const randomSongs = `https://api.deezer.com/artist/${artist.id}/top?limit=4`;
 
- const songResponse=await fetch(randomSongs);
- const songs=await songResponse.json();
+  const songResponse = await fetch(randomSongs);
+  const differentSongs = await songResponse.json();
+console.log(differentSongs)
 
- const song=songs.data.map((song)=>({
-  id: song.id,
-  title: song.title,
-  release_date: song.release_date,
-  link: song.link
- }));
- return song;
+  const song = differentSongs.map((song) => ({
+    id: song.id,
+    title: song.title,
+    release_date: song.release_date,
+    link: song.link
+  }));
+  
+  return song;
 }
-
 
 // "https://api.deezer.com/artist/246791/top?limit=4"
 
-
-seedDatabase();
+seedDatabase(); 
