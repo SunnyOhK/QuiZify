93118be43ba4fb668228d70aac310b767ab4c55e
Bradfh <128874401+Bradfh@users.noreply.github.com>
2023-05-17 21:37:09 -0500

    Round 1 of the quiz is working

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -2,42 +2,60 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('isomorphic-fetch');
 const Artist = require('../models/artist');
+const Song = require('../models/song');
 
 
-
+// this will fetch a random set of 4 artists from the database
 async function getRandomArtists() {
 	try {
 		const artists = await Artist.findAll();
+		console.log('Retrieved Artists:', artists);
 		const shuffledArtists = shuffleArray(artists);
 		const randomArtists = shuffledArtists.slice(0, 4);
-		console.log(randomArtists);
+		console.log('Random Artists', randomArtists);
 		return randomArtists.map(artist => artist.toJSON());
 	} catch (err) {
 		throw new Error('Error getting artists');
 	}
 }
 
+// !THIS WORKS DON'T DELETE.
+// async function getPreviewTrack(artistID) {
+// 	try {
+// 		console.log('Artist ID:', artistID);
+// 		const response = await fetch(`https://api.deezer.com/artist/${artistID}/top?limit=5`);
+// 		console.log('API Response:', response);
+// 		if (!response.ok) {
+// 			throw new Error('Error getting preview track');
+// 		}
+// 		const data = await response.json();
+// 		const topTracks = data.data;
+// 		const previewTracks = data.data.map(track => track.preview);
+// 		const randomTrack = Math.floor(Math.random() * previewTracks.length);
+// 		const previewTrackUrl = previewTracks[randomTrack];
+// 		console.log('Preview Track URL:', previewTrackUrl);
+// 		return previewTrackUrl;
+// 	} catch (err) {
+// 		throw new Error('Error getting preview track');
+// 	}
+// }
+
+// this will fetch the song associated with the random artist from our database
 async function getPreviewTrack(artistID) {
 	try {
-		console.log('Artist ID:', artistID);
-		const response = await fetch(`https://api.deezer.com/artist/${artistID}/top?limit=5`);
-		console.log('API Response:', response);
-		if (!response.ok) {
-			throw new Error('Error getting preview track');
+		const song = await Song.findOne({ where: { artist_id: artistID } });
+		if (!song) {
+			throw new Error('Your song cannot be found');
 		}
-		const data = await response.json();
-		const topTracks = data.data;
-		const previewTracks = data.data.map(track => track.preview);
-		const randomTrack = Math.floor(Math.random() * previewTracks.length);
-		const previewTrackUrl = previewTracks[randomTrack];
+		const previewTrackUrl = song.preview_track_url;
 		console.log('Preview Track URL:', previewTrackUrl);
 		return previewTrackUrl;
 	} catch (err) {
-		throw new Error('Error getting preview track');
+		throw new Error('Error detected.  Cannot get preview track');
 	}
 }
 
-
+//this is the Fisher-Yates shuffle algorithm
 function shuffleArray(array) {
 	const shuffledArray = [...array];
 	for (let i = shuffledArray.length - 1; i > 0; i--) {
@@ -47,11 +65,13 @@ function shuffleArray(array) {
 	return shuffledArray;
 }
 
+// this is our GET route that handles rendering the gameArtistName.handlebars template
 router.get('/', async (req, res) => {
 	try {
 		const artists = await getRandomArtists();
 		const randomArtist = artists[Math.floor(Math.random() * artists.length)];
 		const previewTrackUrl = await getPreviewTrack(randomArtist.id);
+		console.log('Preview Track URL:', previewTrackUrl);
 
 
 		res.render('gameArtistName', { artists, previewTrackUrl });
--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,40 +1,79 @@
+let isPlaying = false;
+let timerInterval = null;
+let remainingTime = 30;
 
 function updateAudioSource(previewTrackUrl) {
   const audio = document.getElementById('audioPlayer');
   audio.src = previewTrackUrl;
 }
 
+function togglePlay() {
+  const audio = document.getElementById('audioPlayer');
+  if (isPlaying) {
+    audio.pause();
+  } else {
+    audio.play();
 
-function fetchPreviewTrackUrl() {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          const response = xhr.responseText;
-          const previewTrackUrl = JSON.parse(response);
-          console.log('Fetched preview track URL:', previewTrackUrl);
-          resolve(previewTrackUrl);
-        } else {
-          reject(new Error('Failed to fetch preview track URL'));
-        }
+    if (!timerInterval) {
+      startTimer();
+    }
+    isPlaying = !isPlaying;
+
+    const playButton = document.getElementById('play-button');
+    playButton.textContent = isPlaying ? 'Pause' : 'Play';
+  }
+
+  function startTimer() {
+    const timerDisplay = document.getElementById('timer');
+    timerDisplay.textContent = remainingTime;
+
+    timerInterval = setInterval(() => {
+      remainingTime--;
+      if (remainingTime >= 0) {
+        timerDisplay.textContent = remainingTime;
+      } else {
+        clearInterval(timerInterval);
       }
-    };
-
-    xhr.open('GET', '/quiz');
-    xhr.send();
-    console.log('Fetching preview track URL...');
-  });
-}
-
-
-async function init() {
-  try {
-    const previewTrackUrl = await fetchPreviewTrackUrl();
-    updateAudioSource(previewTrackUrl);
-  } catch (error) {
-    console.error(error);
+    }, 1000);
   }
 }
 
-init();
+
+  function fetchPreviewTrackUrl() {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            const response = xhr.responseText;
+            const previewTrackUrl = JSON.parse(response);
+            console.log('Fetched preview track URL:', previewTrackUrl);
+            resolve(previewTrackUrl);
+          } else {
+            reject(new Error('Failed to fetch preview track URL'));
+          }
+        }
+      };
+
+      xhr.open('GET', '/quiz');
+      xhr.send();
+      console.log('Fetching preview track URL...');
+    });
+  }
+
+
+  async function init() {
+    try {
+      const previewTrackUrl = await fetchPreviewTrackUrl();
+      updateAudioSource(previewTrackUrl);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const playButton = document.getElementById('play-button');
+  playButton.addEventListener('click', togglePlay);
+
+  init();
+
+
