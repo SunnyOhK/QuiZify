329de30406a04902ba3169a4d4560fa2460d9708
Bradfh <128874401+Bradfh@users.noreply.github.com>
2023-05-19 14:33:29 -0500

    Signup works and POSTs to DB

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 // const loginRoutes = require('./loginRoutes');
 // const logoutRoutes = require('./logoutRoutes');
@@ -8,9 +8,9 @@ const songRoutes=require('./songRoutes')
 const playAgain = require('./playAgain');
 
 
+router.use('/api', apiRoutes);
 router.use('/songs', songRoutes);
 router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
 // router.use('/auth/deezer', loginRoutes);
 // router.use('/logout', logoutRoutes);
 router.use('/quiz', quizController);
