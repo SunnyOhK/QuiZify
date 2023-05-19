d6570544e0ebb734cbf2d102285a663c100d5ee1
Bradfh <128874401+Bradfh@users.noreply.github.com>
2023-05-19 15:17:11 -0500

    login working now too

--- public/js/home.js
+++ public/js/home.js
@@ -5,27 +5,50 @@
 //   window.location.href = '/auth/deezer';
 // });
 
-document.querySelector('#signup-form').addEventListener('submit', function (event) {
-  event.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#signup-form').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-  const formData = Object.fromEntries(new FormData(event.target));
+    const formData = Object.fromEntries(new FormData(event.target));
 
-  fetch('/api/signup', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(formData)
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
+    fetch('/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
     })
-    .catch(error => {
-      console.log('Error:', error);
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log('Error:', error);
 
-    });
+      });
+  });
+
+  document.querySelector('.login-form').addEventListener('submit', function (event) {
+    console.log('Form submitted!');
+    event.preventDefault();
+
+    const formData = Object.fromEntries(new FormData(event.target));
+
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
+  });
 });
-
 
 
--- public/js/login.js
+++ public/js/login.js
@@ -1,53 +1,22 @@
-const loginFormHandler = async (event) => {
+
+document.querySelector('.login-form').addEventListener('submit', function (event) {
+  console.log('Form submitted!');
   event.preventDefault();
 
-  // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const formData = Object.fromEntries(new FormData(event.target));
 
-  if (email && password) {
-    // Send a POST request to the API endpoint
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+  fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log('Error:', error);
     });
-
-    if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
-document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
-
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+});
